a8eabfd52e51705b48d7160bbbdce60ece81fbfe
jsdnlb <danielb.alzate1@gmail.com>
2020-11-22 12:15:10 -0500

    Cambian la ruta inicial

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,33 @@
-import HomeView from '@/components/views/home-view.vue';
-
-export default new Router({
-    routes: [
-        { 
-            path: 'my/shop/detail/go/www/43544352/app/home', 
-            name: 'home',
-            component: HomeView 
-        }
-    ]
+// componentes
+var Home = {
+  template: '<h2>Home</h2>'
+}
+ 
+var AboutUs = {
+  template: '<h2>About Us</h2>'
+}
+ 
+var Contact = {
+  template: '<h2>Contact</h2>'
+}
+ 
+var NotFound = {
+  template: '<h2>Not Found</h2>'
+}
+ 
+// indicamos las rutas con el componente correspondiente
+var router = new VueRouter({
+  history: false,
+  routes: [
+    { path: '/', name: 'home', component: Home },
+    { path: '/about-us', name: 'about-us', component: AboutUs },
+    { path: '/contact', name: 'contact', component: Contact },
+    { path: '*', name: 'not-found', component: NotFound }
+  ],
+});
+ 
+// añadimos router a la instancia de Vue
+new Vue({
+  el: '#app',
+  router: router
 });
